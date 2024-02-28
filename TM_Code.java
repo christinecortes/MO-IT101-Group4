abec10e4dc97d0fa12b0fe237bdfa427714aa165
tmm001 <161644423+tmm001@users.noreply.github.com>
2024-02-28 19:48:42 +0000

    Add files via upload

---
+++ TM_Code.java
@@ -0,0 +1,17 @@
+public class EmployeePresentation {
+    public static void main(String[] args) {
+        String EmployeeID = "001";
+        System.out.println("Employee ID: " + EmployeeID);
+        System.out.println("Employee Last Name: Garcia");
+        System.out.println("Employee First Name: Manuel III");
+        System.out.println("Birthday: 10/11/1983");
+        System.out.println("Address: Valero Carpark Building Valero Street 1227, Makati City");
+        System.out.println("SSS: 44-4506057-3");
+        System.out.println("PhilHealth: 820126853951");
+        System.out.println("TIN: 442-605-657-000");
+        System.out.println("PAG-IBIG: 691295330870");
+        System.out.println("Employee Status: Regular");
+        System.out.println("Employee Position: Chief Executive Officer (CEO)");
+        System.out.println("Immediate Supervisor: None");
+    }
+}
